a8f5e7f74b5d90007925de26d815a43350dfd9f6
oofmaster69420 <57970114+oofmaster69420@users.noreply.github.com>
2021-01-05 20:11:52 -0500

    Update run_superlowspec.sh

--- run_superlowspec.sh
+++ run_superlowspec.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # James Chambers - V1.0 - March 24th 2018
 # Marc Tönsing - V1.2 - September 16th 2019
-# Modified by Joey Reinhart
-# Minecraft Server startup script using screen
+# Modified by Joey Reinhart on Jan 5th 2021
+# Minecraft Server super low spec startup script using screen
 echo "Starting Minecraft server.  To view window type screen -r minecraft."
 echo "To minimize the window and let the server run in the background, press Ctrl+A then Ctrl+D"
 cd /home/pi/minecraft/
