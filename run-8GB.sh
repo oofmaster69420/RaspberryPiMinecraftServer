97d17479975694017d17c93232d55eaff331221c
oofmaster69420 <57970114+oofmaster69420@users.noreply.github.com>
2021-07-14 13:36:07 -0400

    Update run-8GB.sh

--- run-8GB.sh
+++ run-8GB.sh
@@ -3,8 +3,7 @@
 # Marc Tönsing - V1.2 - September 16th 2019
 # Modified by Joey Reinhart on Jan 5th 2021
 # Minecraft Server startup script using screen
-user=$(whoami)
 echo "Starting Minecraft server.  To view window type screen -r minecraft."
 echo "To minimize the window and let the server run in the background, press Ctrl+A then Ctrl+D"
-cd /home/$user/minecraft/
-/usr/bin/screen -dmS minecraft /usr/bin/java -jar -Xms5500M -Xmx5500M /home/$user/minecraft/server.jar
+cd /home/$USER/minecraft/
+/usr/bin/screen -dmS minecraft /usr/bin/java -jar -Xms5500M -Xmx5500M /home/$USER/minecraft/server.jar
